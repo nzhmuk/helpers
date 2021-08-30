a0121c1f13808460db4e6ccec331ca51c1ada3bf
Nikolay Zhmuk <nzhmuk@plesk.com>
2021-08-30 18:02:29 +0700

    added create_subscription function

--- plesk_items_generator.sh
+++ plesk_items_generator.sh
@@ -2,3 +2,19 @@
 
 set -o errexit
 set -o pipefail
+
+function create_subscriptions() {
+	
+	printf "Enter a quantity of subscriptions to be created: \n"
+	read sub_quantity
+	for i in {1.."$sub_quantity"} 
+		do 
+			plesk bin subscription --create subscription$1.test -owner admin -service-plan "Default Domain" -login clone$1 -passwd "1qazXSW@1qazXSW@" 
+		done
+}
+
+function main() {
+	create_subscriptions
+}
+
+main "$@"
