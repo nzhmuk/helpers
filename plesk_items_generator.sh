ce049ba7e9a354b55a2259f5ac39d533bd3df2fd
Nikolay Zhmuk <nzhmuk@plesk.com>
2021-09-16 22:23:23 +0700

    fixed for loop in create_subscription function

--- plesk_items_generator.sh
+++ plesk_items_generator.sh
@@ -7,44 +7,44 @@ IPV4_REGEX="^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$"
 SUBSCRIPTION_IP=0
 
 function get_subscription_ip() {
-                ip_addresses=( $(hostname -I) )
-                ip_addresses_quantity=${#ip_addresses[@]}
-                index=0
 
-                printf "List of available IPv4 addresses on the server [IP address - index]:\n"
-                while [ "$index" -lt "$ip_addresses_quantity" ]
-                do
-                    if [[ ${ip_addresses[$index]} =~ $IPV4_REGEX ]]; then
-                        echo "${ip_addresses[$index]} - $index "
-                    fi
-                        let "index = $index + 1"
-                done
+        ip_addresses=( $(hostname -I) )
+        ip_addresses_quantity=${#ip_addresses[@]}
+        index=0
 
-                printf "Enter index for the selected IP address: \n"
-                read selected_id
-                SUBSCRIPTION_IP="${ip_addresses[$selected_id]}"
+        printf "List of available IPv4 addresses on the server [IP address - index]:\n"
+        while [ "$index" -lt "$ip_addresses_quantity" ]
+        do
+			if [[ ${ip_addresses[$index]} =~ $IPV4_REGEX ]]; then
+				echo "${ip_addresses[$index]} - $index "
+			fi
+            let "index = $index + 1"
+        done
+
+        printf "Enter index for the selected IP address: \n"
+        read selected_id
+        SUBSCRIPTION_IP="${ip_addresses[$selected_id]}"
 }
 
 function create_subscriptions() {
 
         printf "Enter a quantity of subscriptions to be created: \n"
         read sub_quantity
-		printf "Enter prefix for subscriptions name (latin, no spaces and special characters): \n"
-		read sub_prefix
-		printf "Enter a prefix for sysuser (latin, no spaces and special characters): \n"
-		read sub_sysuser
-		
-        for i in {1.."$sub_quantity"}
-                do
-                        plesk bin subscription --create $sub_prefix$i.test -owner admin -service-plan "Default Domain" -ip $SUBSCRIPTION_IP -login $sub_sysuser$i -passwd "1qazXSW@1qazXSW@"
-                done
+        printf "Enter prefix for subscriptions name (latin, no spaces and special characters): \n"
+        read sub_prefix
+        printf "Enter a prefix for sysuser (latin, no spaces and special characters): \n"
+        read sub_sysuser
+
+
+        for i in $(seq 1 "$sub_quantity")
+        do
+            plesk bin subscription --create $sub_prefix$i.test -owner admin -service-plan "Default Domain" -ip $SUBSCRIPTION_IP -login $sub_sysuser$i -passwd "1qazXSW@1qazXSW@"
+        done
 }
 
 function main() {
         get_subscription_ip
         create_subscriptions
-
-
 }
 
 main "$@"
